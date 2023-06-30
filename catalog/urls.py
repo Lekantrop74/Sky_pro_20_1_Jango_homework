f460d937f1b054891d2df4eccf6b824e70ab0bda
Korolev_Viktor <Lekantrop74@yandex.ru>
2023-06-30 12:14:00 +0500

    convert fbv in cbv in Product and fix structure project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from catalog.views import index, contact, home_page, product_list, add_data, ContactCreateView
+from catalog.views import index, contact, home_page, ContactCreateView, AddDataView, ProductListView, navbar
 
 urlpatterns = [
     path('home_page/', home_page, name='home_page'),
     path('', RedirectView.as_view(url='/home_page/', permanent=True)),
+    path('navbar/', navbar, name='navbar'),
+
     path('index/', index, name='index'),
     path('contact_page/contact/', contact, name='contact'),
     path('create/', ContactCreateView.as_view(), name='create_contact'),
-    path('base/',  product_list, name='product_list_base'),
-    path('base/add_data/', add_data, name='add_data'),
+    path('base/product_add_data/', AddDataView.as_view(), name='add_data'),
+    path('base/', ProductListView.as_view(), name='product_list_base'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,16 +3,23 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from .models import Contact, Product, Category
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'catalog/home_page.html')
+    # Отображение домашней страницы
+    return render(request, 'catalog/base_page/home_page.html')
 
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    # Отображение индексной страницы
+    return render(request, 'catalog/base_page/index.html')
+
+
+def navbar(request):
+    # Отображение навигационной панели
+    return render(request, 'catalog/base_page/navbar.html')
 
 
 class ContactListView(ListView):
@@ -25,6 +32,7 @@ class ContactListView(ListView):
 
 def contact(request):
     contact_list_view = ContactListView.as_view()
+    # Обработчик запроса для страницы контактов
     return contact_list_view(request)
 
 
@@ -34,45 +42,47 @@ class ContactCreateView(CreateView):
     success_url = reverse_lazy('contact')
 
 
-def add_data(request):
-    if request.method == 'POST':
-        # Retrieve the form data from the POST request
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        preview_image = request.FILES.get('preview_image')
-        category_id = request.POST.get('category')
-        price = request.POST.get('price')
+class AddDataView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'preview_image', 'category', 'price')
+    template_name = 'catalog/product_page/product_add_data.html'
+    success_url = '/base'
 
-        # Create a new Product instance with the retrieved data
-        product = Product(
-            name=name,
-            description=description,
-            preview_image=preview_image,
-            category_id=category_id,
-            price=price
-        )
-
-        # Save the product to the database
-        product.save()
-
-        # Redirect to a success page or a different URL
-        return redirect('/base')  # Replace 'success_page' with your desired URL name
-
-    # If it's a GET request or there are validation errors, render the form template
-    categories = Category.objects.all()
-    context = {'categories': categories}
-    return render(request, 'catalog/add_data.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        # Получение всех категорий товаров и добавление их в контекст
+        return context
 
 
 def base(request):
-    return render(request, 'catalog/base.html')
+    # Отображение базовой страницы продуктов
+    return render(request, 'catalog/product_page/product_base.html')
 
 
-def product_list(request):
-    products = Product.objects.all()
-    paginator = Paginator(products, 5)  # Разбиваем список на страницы по 5 объектов на каждой
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 5
+    template_name = 'catalog/product_page/product_list.html'
+    context_object_name = 'page_obj'
 
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    def get_queryset(self):
+        return Product.objects.order_by('-created_at')
+        # Получение списка продуктов, отсортированных по убыванию даты создания
 
-    return render(request, 'catalog/product_list.html', {'page_obj': page_obj})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_obj'] = Paginator(self.get_queryset(), self.paginate_by).get_page(self.request.GET.get('page'))
+        # Добавление пагинированного списка продуктов в контекст
+        return context
+
+
+# def product_list(request):
+#     # стандартная пагинация не удалил чтобы не искать потом код если нужно будет сделать не через классы
+#     products = Product.objects.all()
+#     paginator = Paginator(products, 5)  # Разбиваем список на страницы по 5 объектов на каждой
+#
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#
+#     return render(request, 'catalog/product_page/product_list.html', {'page_obj': page_obj})
